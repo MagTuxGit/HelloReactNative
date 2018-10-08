48a957308952d85f2e4dc16da54e9abb678f72dc
Andriy Trubchanin <magtuxgit@gmail.com>
2018-10-08 14:07:25 +0300

    touchables

--- App.js
+++ App.js
@@ -1,6 +1,11 @@
 //ES2015
 import React, { Component } from 'react';
-import { Text, View, Image, StyleSheet, ScrollView,  TextInput, Button, Alert } from 'react-native';
+import { 
+  Text, View, Image, StyleSheet, ScrollView,  
+  TextInput, Button, Alert,
+  Platform,
+  TouchableHighlight, TouchableOpacity, TouchableNativeFeedback, TouchableWithoutFeedback  
+} from 'react-native';
 
 class NavigationBar extends Component {
   render() {
@@ -204,6 +209,55 @@ class ButtonBasics extends Component {
   }
 }
 
+class Touchables extends Component {
+  _onPressButton() {
+    Alert.alert('You tapped the button!')
+  }
+
+  _onLongPressButton() {
+    Alert.alert('You long-pressed the button!')
+  }
+
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <TouchableHighlight onPress={this._onPressButton} underlayColor="white">
+          <View style={styles.button}>
+            <Text style={styles.buttonText}>TouchableHighlight</Text>
+          </View>
+        </TouchableHighlight>
+        <TouchableOpacity onPress={this._onPressButton}>
+          <View style={styles.button}>
+            <Text style={styles.buttonText}>TouchableOpacity</Text>
+          </View>
+        </TouchableOpacity>
+        {/*
+        <TouchableNativeFeedback
+            onPress={this._onPressButton}
+            background={Platform.OS === 'android' ? TouchableNativeFeedback.SelectableBackground() : ''}>
+          <View style={styles.button}>
+            <Text style={styles.buttonText}>TouchableNativeFeedback</Text>
+          </View>
+        </TouchableNativeFeedback>
+        */}
+        <TouchableWithoutFeedback
+            onPress={this._onPressButton}
+            >
+          <View style={styles.button}>
+            <Text style={styles.buttonText}>TouchableWithoutFeedback</Text>
+          </View>
+        </TouchableWithoutFeedback>
+        <TouchableHighlight onPress={this._onPressButton} onLongPress={this._onLongPressButton} underlayColor="white">
+          <View style={styles.button}>
+            <Text style={styles.buttonText}>Touchable with Long Press</Text>
+          </View>
+        </TouchableHighlight>
+      </View>
+    );
+  }
+}
+
 export default class HelloWorldApp extends Component {
   render() {
     let pic = {
@@ -251,6 +305,7 @@ export default class HelloWorldApp extends Component {
             />          
             */}
             <ButtonBasics />
+            <Touchables />
         </View>
       </ScrollView>
       </View>
@@ -260,31 +315,42 @@ export default class HelloWorldApp extends Component {
 
 const styles = StyleSheet.create({
   bigblue: {
-    color: 'blue',
-    fontWeight: 'bold',
-    fontSize: 30,
+    color: "blue",
+    fontWeight: "bold",
+    fontSize: 30
   },
   red: {
-    color: 'red',
+    color: "red"
   },
   header: {
-    fontWeight: 'bold',
+    fontWeight: "bold",
     fontSize: 16,
-    textAlign: 'center',
+    textAlign: "center",
     paddingTop: 16,
     backgroundColor: "white"
   },
 
   container: {
     flex: 1,
-    justifyContent: 'center',
-   },
-   buttonContainer: {
-     margin: 8
-   },
-   alternativeLayoutButtonContainer: {
-     margin: 20,
-     flexDirection: 'row',
-     justifyContent: 'space-between'
-   }  
+    justifyContent: "center"
+  },
+  buttonContainer: {
+    margin: 8
+  },
+  alternativeLayoutButtonContainer: {
+    margin: 20,
+    flexDirection: "row",
+    justifyContent: "space-between"
+  },
+
+  button: {
+    marginBottom: 30,
+    width: 260,
+    alignItems: "center",
+    backgroundColor: "#2196F3"
+  },
+  buttonText: {
+    padding: 20,
+    color: "white"
+  }
 });
